9242791c62912afdd769baa97d560ae08cba239b
d-002 <leo.daloz@epita.fr>
2026-04-19 00:07:17 +0200

    compare_estimators: using more datasets from openml

--- testing/compare_estimators.py
+++ testing/compare_estimators.py
@@ -9,31 +9,46 @@ import matplotlib.pyplot as plt
 from time import time
 from sklearn.metrics import f1_score
 from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.datasets import make_classification, make_moons, \
-        load_breast_cancer
+from sklearn.datasets import load_breast_cancer, load_diabetes, load_wine, \
+        make_classification, make_moons, fetch_openml
 
 from sklearn_morpho.classifiers.ldep import LDEP
 
 # set up estimators and datasets
 random_state = np.random.RandomState(11)
 estimators = {
-    'l_DEP': LDEP(),
-    'RBF SVC': SVC(kernel='rbf'),
-    'Poly SVC': SVC(kernel='poly'),
-    'Linear SVC': SVC(kernel='linear'),
-    'MLP': MLPClassifier(),
+    'l_DEP': LDEP(random_state=random_state),
+    'RBF SVC': SVC(kernel='rbf', random_state=random_state),
+    'Poly SVC': SVC(kernel='poly', random_state=random_state),
+    'Linear SVC': SVC(kernel='linear', random_state=random_state),
+    'MLP': MLPClassifier(max_iter=1000, random_state=random_state),
 }
+
+def get_clean_X_y_openml(name: str) -> tuple[np.ndarray, np.ndarray]:
+    data = fetch_openml(name, version=1, as_frame=True)
+    X = data.data.select_dtypes(include=[np.number]).fillna(0).values
+    le = LabelEncoder()
+    y = le.fit_transform(data.target)
+
+    return X, y
+
+def make_binary(X: np.ndarray, y: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    y = (y > np.median(y))
+    return X, y
 
 datasets = {
     'normal classification': make_classification(
         n_samples=500, n_features=2, n_redundant=0, n_classes=2,
         n_clusters_per_class=1, random_state=random_state),
-    'breast cancer': load_breast_cancer(return_X_y=True),
     'non noisy moons': make_moons(n_samples=500, random_state=random_state),
     'noisy moons': make_moons(n_samples=500, noise=.2,
                               random_state=random_state),
+    'breast cancer': load_breast_cancer(return_X_y=True),
+    'diabetes': make_binary(*load_diabetes(return_X_y=True)),
+    'wine': make_binary(*load_wine(return_X_y=True)),
 }
 
 # evaluate estimators
