6b789c3d3f4924503ad5ec13ee5645ffb9bdb754
d-002 <leo.daloz@epita.fr>
2026-04-28 10:58:39 +0200

    testing: fixed parameters for check_estimator

--- src/sklearn_morpho/tests/scikit_test.py
+++ src/sklearn_morpho/tests/scikit_test.py
@@ -1,6 +1,14 @@
 from sklearn.utils.estimator_checks import check_estimator
 
 from sklearn_morpho.classifiers.ldep import LDEP
+from sklearn_morpho.stopping.stopping_cost import CostStoppingMethod
+from sklearn_morpho.stopping.stopping_iter import IterStoppingMethod
 
 def test_check_estimator():
-    assert check_estimator(LDEP())
+    ldep = LDEP(validation_ratio=0,
+                stopping_methods=[
+                    CostStoppingMethod(1e-6),
+                    IterStoppingMethod(100),
+                ])
+
+    assert check_estimator(ldep)
