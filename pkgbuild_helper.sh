7aa7b392353b7f2c810ee8bfd7e1825d8cc1c980
d-002 <mailofd00@gmail.com>
2026-05-22 18:59:58 +0200

    aur: updated pkgbuild helper, published to the AUR

--- pkgbuild_helper.sh
+++ pkgbuild_helper.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
+pkg_name="$(basename -- "$(pwd)")"
+files=("PKGBUILD" ".SRCINFO")
+
+if [ -e "python-$pkg_name" ]; then
+    echo "Error: aur repo already cloned, please save your work and remove"
+    exit 1
+fi
+
 updpkgsums
 makepkg --printsrcinfo > .SRCINFO
+
+git clone "ssh://aur@aur.archlinux.org/python-$pkg_name.git"
+cd "python-$pkg_name"
+
+for file in "${files[@]}"; do
+    cp "../$file" .
+done
