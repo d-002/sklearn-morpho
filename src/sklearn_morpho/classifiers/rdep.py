15abc20124790c30947cb1b31ba2dcd1543976ac
d-002 <mailofd00@gmail.com>
2026-05-29 00:23:11 +0200

    rdep: add basic nonfunctional classes

---
+++ src/sklearn_morpho/classifiers/rdep.py
@@ -0,0 +1,156 @@
+import numpy as np
+from typing import Literal, cast
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils import check_random_state
+from sklearn.utils.validation import validate_data, check_is_fitted
+from sklearn.utils.multiclass import unique_labels
+
+from ..dccp.dccp_rdep import RDEPDccpTrainer
+from ..stopping import (
+        StoppingMethod,
+        CostStoppingMethod,
+        EarlyStoppingMethod,
+        EpochStoppingMethod,
+        TrainStopStoppingMethod,
+)
+from ..weighting import SampleWeighting, NoneSampleWeighting
+
+class RDEP(ClassifierMixin, BaseEstimator):
+    """
+    Scikit-learn estimator wrapper around a r-DEP (reduced Dilation-Erosion
+    morphological Perceptron) for binary data classification.
+
+    r-DEP forward pass equation:
+
+    \\[ y = f(\\lambda \\tau_(x) + (1 - \\lambda) \\tau'_(x)) \\]
+
+    Where $\\tau$ refers to the activation of a (max, +) morphological
+    perceptron and $\\tau'$ to a (min, +) one.
+    """
+
+    def __init__(self, margin = 1., validation_ratio = .3,
+                 weighting_method: SampleWeighting | None = None,
+                 stopping_methods: list[StoppingMethod] | None = None,
+                 verbose: Literal[0, 1, 2] = 0,
+                 random_state: np.random.RandomState | None = None) -> None:
+        """
+        Initialize the classifier, see class help for more.
+
+        param margin:           Enforce a margin between the decision boundary
+                                and the data. May help with linearly separable
+                                datasets, but generally lower is more accurate.
+        param validation_radio: How much of the training set to dedicate to use
+                                as validation during fitting.
+                                Must be between 0 and 1 (inclusive, exclusive),
+                                if set to exactly 0 then incompatible stopping
+                                methods cannot be used (e.g. early stopping).
+        param weighting_method: The weighting method to use: apply weights to
+                                the cost contribution of each data point to help
+                                avoid outliers.
+                                If left to None, will use NoneWeightingMethod()
+        param stopping_methods: A list of stopping methods, must not be empty.
+                                At each epoch, these methods will be
+                                sequentially asked whether the training should
+                                stop. In this case, epoch ends by rolling back
+                                to the epoch with the best validation cost.
+                                If left to None, will use:
+                                [
+                                    CostStoppingMethod(1e-6),
+                                    EarlyStoppingMethod(5),
+                                    EpochStoppingMethod(20),
+                                    TrainStopStoppingMethod(),
+                                ]
+        param verbose:          Whether to log extra information. 0: no logging,
+                                1: basic logging / timing, 2: cvxpy solve() set
+                                to verbose mode.
+        param random_state:     A RandomState object or None to allow for seeded
+                                randomness.
+        """
+
+        self.margin = margin
+        self.validation_ratio = validation_ratio
+        self.weighting_method = weighting_method
+        self.stopping_methods = stopping_methods
+        self.verbose: Literal[0, 1, 2] = verbose
+        self.random_state = random_state
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> RDEP:
+        """
+        Fit the classifier, create attributes:
+        - self.max_perceptron_
+        - self.min_perceptron_
+        - self.lambda_
+        - self.classes_:        Unique labels generated from y
+
+        X and y must represent binary classifiable data.
+        """
+
+        # input data validation
+        random_state = check_random_state(self.random_state)
+        X, y = validate_data(self, X, y)
+        self.scaler_ = StandardScaler()
+        X_scaled = self.scaler_.fit_transform(X)
+
+        # set unset parameters to their default values
+        if self.weighting_method is None:
+            weighting_method = NoneSampleWeighting()
+        else:
+            weighting_method = self.weighting_method
+        if self.stopping_methods is None:
+            stopping_methods = [
+                CostStoppingMethod(1e-6),
+                EarlyStoppingMethod(5),
+                EpochStoppingMethod(20),
+                TrainStopStoppingMethod(),
+            ]
+        else:
+            stopping_methods = self.stopping_methods
+
+        # create classes and convert them to distinct integers for fitting
+        # the classes are persisted inside the object for use in predict
+        self.classes_ = unique_labels(y)
+        classes_list = list(self.classes_)
+        y_integers = np.array([classes_list.index(c) for c in y],
+                           dtype=np.int32)
+
+        if len(classes_list) != 2:
+            raise ValueError('Only binary classification is supported but '
+                             f'got {len(classes_list)} class(es).')
+
+        # create and train perceptrons
+        trainer = RDEPDccpTrainer(self.margin, self.validation_ratio,
+                                  weighting_method, stopping_methods,
+                                  self.verbose, random_state)
+
+        trainer.train(X_scaled, y_integers)
+        self.max_perceptron_ = trainer.max_perceptron
+        self.min_perceptron_ = trainer.min_perceptron
+        self.lambda_ = trainer.lambda_
+
+        return self
+
+    def decision_function(self, X: np.ndarray) -> np.ndarray:
+        check_is_fitted(self)
+        X = validate_data(self, X, reset=False)
+        X_scaled = cast(np.ndarray, self.scaler_.transform(X))
+
+        expr_max = np.max(self.max_perceptron_ + X_scaled, axis=1)
+        expr_min = np.min(self.min_perceptron_ + X_scaled, axis=1)
+        return expr_max * self.lambda_ + expr_min * (1 - self.lambda_)
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        check_is_fitted(self)
+        return self.classes_[(self.decision_function(X) >= 0).astype(int)]
+
+    def __sklearn_tags__(self):
+        """
+        Overriden method to allow check_estimator to not run accuracy tests.
+        These are designed for perceptrons with a linear decision boundary,
+        which is not the case for a morphological perceptron.
+        """
+
+        tags = super().__sklearn_tags__()
+        tags.classifier_tags.multi_class = False
+        return tags
---
+++ src/sklearn_morpho/dccp/dccp_rdep.py
@@ -0,0 +1,127 @@
+from typing import Literal
+import numpy as np
+import cvxpy as cp
+
+from .dccp_wrapper import DccpTrainer
+from ..weighting import SampleWeighting
+from ..stopping import StoppingMethod
+
+class LDEPDccpTrainer(DccpTrainer):
+    def __init__(self, margin: float, validation_ratio: float,
+                 weighting_method: SampleWeighting,
+                 stopping_methods: list[StoppingMethod],
+                 verbose: Literal[0, 1, 2],
+                 random_state: np.random.RandomState) -> None:
+        """
+        Initialize the r-DEP trainer.
+
+        param [any]:    see base class
+        """
+
+        super().__init__(margin, validation_ratio, weighting_method,
+                         stopping_methods, verbose, {}, random_state)
+
+    def at_training_start(self, data_dim: int) -> None:
+        self._objective = None
+
+        # Extracted parameters that will be populated during training but need
+        # initial values for linearization
+        self.max_perceptron = self.random_state.randn(data_dim)
+        self.min_perceptron = self.random_state.randn(data_dim)
+
+        # Create constraints for linearization derived from real parameters,
+        # used to absorbe non-convex parameters that are restored at the end.
+        # Method inspired by arXiv:2011.06512v1.
+        # TODO: is this representation an issue since the final lambda parameter
+        # is not necessarily in [0, 1]?
+        self._max_training_weights = cp.Variable(data_dim)
+        self._min_training_weights = cp.Variable(data_dim)
+
+    def get_problem(self, X: np.ndarray, y: np.ndarray,
+                    cost_weights: np.ndarray) -> cp.Problem:
+        K = X.shape[0]
+
+        # the objective and the slack variables do not change, cache them
+        if self._objective is None:
+            self._slack = cp.Variable(K)
+            self._objective = cp.Minimize(
+                    cp.sum(cp.multiply(cp.pos(self._slack), cost_weights)))
+
+        # Constraints: convex constraints are for data points in the first
+        # class, while concave ones are for points in the second class.
+        # Therefore, linearize things when needed to make the problem convex,
+        # sometimes using values from the previous epoch.
+
+        idx_max = np.argmax(self.max_perceptron + X @ self.max_matrix.T,
+                            axis=1)
+        idx_min = np.argmin(self.min_perceptron + X @ self.min_matrix.T,
+                            axis=1)
+
+        # create arrays to regroup the active indices using numpy, to then apply
+        # constraints all at once and use AST optimizations inside cvxpy
+        M_max = np.zeros((K, self.max_perceptron.size))
+        M_min = np.zeros((K, self.min_perceptron.size))
+        M_max[np.arange(K), idx_max] = 1
+        M_min[np.arange(K), idx_min] = 1
+
+        constraints = []
+        for label in [0, 1]:
+            mask = y == label
+            if not np.any(mask):
+                continue
+
+            X_ = X[mask]
+            K_ = X_.shape[0]
+
+            # start building the perceptron's outputs before the min/max
+            expr_max = X_ @ self._max_training_matrix.T
+            expr_min = X_ @ self._min_training_matrix.T
+
+            # add weights to every row of (X @ matrix.T) using np.ones to create
+            # a matrix safely for cvxpy's cpp backend
+            ones = np.ones((K_, 1))
+            expr_max += ones @ cp.reshape(self._max_training_weights,
+                                          (1, self.max_perceptron.size),
+                                          order='C')
+            expr_min += ones @ cp.reshape(self._min_training_weights,
+                                          (1, self.min_perceptron.size),
+                                          order='C')
+
+            active_max = cp.sum(cp.multiply(M_max[mask], expr_max), axis=1)
+            active_min = cp.sum(cp.multiply(M_min[mask], expr_min), axis=1)
+
+            if label == 0:
+                constraints.append(self._slack[mask] >= self.margin
+                                   + cp.max(expr_max, axis=1) + active_min)
+            else:
+                constraints.append(self._slack[mask] >= self.margin
+                                   - active_max - cp.min(expr_min, axis=1))
+        return cp.Problem(self._objective, constraints)
+
+    def get_cost(self, X: np.ndarray, y: np.ndarray) -> float:
+        expr_max = np.max(self.max_perceptron + X, axis=1)
+        expr_min = np.min(self.min_perceptron + X, axis=1)
+
+        # can use the expressions directly without needing to multiply by lambda
+        # since still in the training phase
+        cost = self.margin + (expr_max + expr_min) * (1 - 2 * y)
+        return np.maximum(0, cost).sum()
+
+    def after_epoch(self) -> None:
+        # update the perceptrons weights from this epoch's results
+        if self._max_training_weights.value is None or \
+                self._min_training_weights.value is None:
+            raise ValueError('CvxPy could not optimize a perceptron')
+
+        self.max_perceptron = self._max_training_weights.value
+        self.min_perceptron = self._min_training_weights.value
+
+    def save_best(self) -> None:
+        self.saved = {
+            'max_w': np.copy(self.max_perceptron),
+            'min_w': np.copy(self.min_perceptron),
+        }
+
+    def rollback_to_best(self) -> None:
+        self.max_perceptron = self.saved['max_w']
+        self.min_perceptron = self.saved['min_w']
