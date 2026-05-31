441fb9ecb6745b1e0f4981a94d913142c39cfa7b
d-002 <mailofd00@gmail.com>
2026-05-31 16:30:02 +0200

    check todos: better blacklist to ignore base64 images in ipynb

--- check_todos.sh
+++ check_todos.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# split word to avoid grepping this very file
-word=$(echo to)$(echo do)
+# ignore ipynb since their base64 images can include "todo"
+ignored="$(basename $0) .ipynb __pycache__"
 
 shopt -s globstar
 code=0
 for file in **/*; do
-    if [ -f "$file" ]; then
+    ignore_this=false
+    for pattern in $ignored; do
+        if [[ "$file" = *"$pattern"* ]]; then
+            ignore_this=true
+            break
+        fi
+    done
+
+    if [ -f "$file" -a $ignore_this == false ]; then
         # split into two commands for coloring
-        grep -i "$word" "$file"
-        if [ -n "$(grep -i "$word" "$file")" ]; then
+        grep -i todo "$file"
+        if [ -n "$(grep -i todo "$file")" ]; then
             code=1
         fi
     fi
