112113a0f7cac863f1aa0169ccf999cf8681c0f7
d-002 <mailofd00@gmail.com>
2026-06-02 16:39:57 +0200

    submodules: add __init__ everywhere

---
+++ src/sklearn_morpho/dccp/__init__.py
@@ -0,0 +1,4 @@
+from .dccp_ldep import LDEPDccpTrainer
+from .dccp_rdep import RDEPDccpTrainer
+from .dccp_simple_perceptron import SimplePerceptronDccpTrainer
+from .dccp_wrapper import DccpTrainer
