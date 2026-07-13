55abab817720e0562e5fc26c8bf8a02605636fa4
d-002 <mailofd00@gmail.com>
2026-07-13 17:16:47 +0200

    feat(kind): add in tests, add str/enum unit tests

--- tests/integration/sklearn_test.py
+++ tests/integration/sklearn_test.py
@@ -1,6 +1,7 @@
 from sklearn.utils.estimator_checks import check_estimator
 
 from sklearn_morpho import LDEP, RDEP, MorphoPerceptron
+from sklearn_morpho.utils import Kind
 
 
 def test_check_estimator_ldep():
@@ -13,11 +14,21 @@ def test_check_estimator_rdep():
     assert check_estimator(rdep)
 
 
-def test_check_estimator_max_perceptron():
+def test_check_estimator_max_perceptron_str():
     perceptron = MorphoPerceptron(kind='max')
     assert check_estimator(perceptron)
 
 
-def test_check_estimator_min_perceptron():
+def test_check_estimator_min_perceptron_str():
     perceptron = MorphoPerceptron(kind='min')
+    assert check_estimator(perceptron)
+
+
+def test_check_estimator_max_perceptron_enum():
+    perceptron = MorphoPerceptron(kind=Kind.MAX)
+    assert check_estimator(perceptron)
+
+
+def test_check_estimator_min_perceptron_enum():
+    perceptron = MorphoPerceptron(kind=Kind.MIN)
     assert check_estimator(perceptron)
