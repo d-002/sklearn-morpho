be6270172a818f19b5365d4959c70768bd46e821
d-002 <mailofd00@gmail.com>
2026-07-13 17:16:33 +0200

    fix(source files): cleaner and fixed __init__ files

--- src/sklearn_morpho/__init__.py
+++ src/sklearn_morpho/__init__.py
@@ -1,9 +1,5 @@
 from .classifiers import (
     LDEP as LDEP,
-)
-from .classifiers import (
     RDEP as RDEP,
-)
-from .classifiers import (
     MorphoPerceptron as MorphoPerceptron,
 )
--- src/sklearn_morpho/classifiers/__init__.py
+++ src/sklearn_morpho/classifiers/__init__.py
@@ -1,6 +1,3 @@
 from .ldep import LDEP as LDEP
 from .rdep import RDEP as RDEP
-from .simple_perceptron import (
-    MorphoPerceptron as MorphoPerceptron,
-    Kind as Kind,
-)
+from .simple_perceptron import MorphoPerceptron as MorphoPerceptron
